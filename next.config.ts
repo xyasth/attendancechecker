85e51c2d1f68dfbb2b2424cf513aaac008d0143e
xyasth <pkosasih@student.ciputra.ac.id>
2025-09-04 12:34:53 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config: Configuration) => {
+    // Force @vladmandic/human to always use browser ESM build
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@vladmandic/human/dist/human.node.js":
+        "@vladmandic/human/dist/human.esm.js",
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
